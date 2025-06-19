2dab99d3f90fd735f9f79e2bbd8cc5bc40325094
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-06-19 16:28:46 +0100

    remove part1 code (for next tutorials and stuff ig)

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,3 @@
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, MenuLayer) {
-public:
-    bool init() {
-        if (!MenuLayer::init()) {
-            return false;
-        }
-
-        auto bottomMenu = this->getChildByID("bottom-menu");
-        auto button = CCMenuItemSpriteExtra::create(
-            CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-            this,
-            menu_selector(MyMenuLayer::onTestButton)
-        );
-
-        return true;
-    }
-
-    void onTestButton(CCObject* sender) {
-        FLAlertLayer::create("Hi!", "You clicked the button :D", "OK")->show();
-    }
-};
