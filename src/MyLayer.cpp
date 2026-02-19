9f2b81d50550b358a1152159ddc8977a250644b0
OmgRod <89850217+OmgRod@users.noreply.github.com>
2026-02-19 11:59:49 +0000

    Create MyLayer.cpp

---
+++ src/MyLayer.cpp
@@ -0,0 +1,42 @@
+#include <Geode/Geode.hpp>
+#include "MyLayer.hpp"
+
+using namespace geode::prelude;
+
+CCScene* MyLayer::scene() {
+    auto layer = MyLayer::create();
+    auto scene = CCScene::create();
+    scene->addChild(layer);
+
+    return scene;
+}
+
+bool MyLayer::init() {
+    if (!CCLayer::init()) return false;
+
+    auto winSize = CCDirector::sharedDirector()->getWinSize();
+       
+    auto background = createLayerBG(); // USING GEODE FUNCTIONS FOR SIMPLICITY
+    background->setColor({ 255, 0, 0 });
+    this->addChild(background);
+
+    auto menu = CCMenu::create();
+    menu->setPosition({ winSize.width * 0.5f, winSize.height * 0.5f });
+    menu->setContentSize({ winSize.width, winSize.height });
+    menu->setAnchorPoint({ 0.f, 0.f });
+    this->addChild(menu);
+
+    auto text = CCLabelBMFont::create("Hello, world!", "bigFont.fnt");
+    text->setPosition({ winSize.width * 0.5f, winSize.height * 0.5f });
+    this->addChild(text);
+
+    auto backBtn = addBackButton(this); // USING GEODE FUNCTIONS FOR SIMPLICITY - onBack FUNCTION NOT NEEDED ANYMORE!
+    backBtn->setPosition({ (0 - (winSize.width / 2)) + 20.f, (winSize.height / 2) - 20.f });
+    menu->addChild(backBtn);
+
+    return true;
+}
+
+void MyLayer::keyBackClicked() {
+    CCDirector::sharedDirector()->popSceneWithTransition(0.5f, PopTransition::kPopTransitionFade);
+}
